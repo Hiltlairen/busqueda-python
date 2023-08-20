659c26846a9a3b97bb0b310ee050d85688562ba1
Hiltlairen <sirjhoswilliam5@gmail.com>
2023-08-20 10:20:52 -0400

    actualizacion

--- grafos.py
+++ grafos.py
@@ -4,6 +4,13 @@ class grafos_c:
     def crando(self):
         ng=str(input("nombre del nodo:"))
         self.GN.append(ng)
+        if len(self.GN)!=0:
+            return
+        else:
+            self.conexiones()
     def ver_nodos(self):
         for i in range (len(self.GN)):
             print((i+1),".-",self.GN[i]) 
+    def conexiones(self):
+        print("holas")
+        
